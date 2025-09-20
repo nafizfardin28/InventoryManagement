690a096108cfac24c9f60f92d493e90ddc1b68b3
Nafiz Mahmud Fardin <bsse1528@iit.du.ac.bd>
2025-09-20 22:19:11 +0600

    2nd commit

--- src/main/java/com/ecommerce/controller/OrderListController.java
+++ src/main/java/com/ecommerce/controller/OrderListController.java
@@ -63,8 +63,7 @@ public class OrderListController {
     @FXML
     private void handleBack() throws IOException {
         Stage stage = (Stage) orderTable.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-                Session.getCurrentRole().equalsIgnoreCase("admin") ? "/com/ecommerce/control_panel.fxml" : "/com/ecommerce/employee_control_panel.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(Session.getCurrentRole().equalsIgnoreCase("admin") ? "/com/ecommerce/control_panel.fxml" : "/com/ecommerce/employee_control_panel.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
     }
